009aadcfb5203d5b10fd8774864a5b68667f8506
David Negreira <david.negreira@canonical.com>
2024-09-19 16:12:04 +0200

    test

--- selftests/unit/utils/software_manager.py
+++ selftests/unit/utils/software_manager.py
@@ -13,12 +13,37 @@ def apt_supported_distro():
     return distro.detect().name in ["debian", "Ubuntu"]
 
 
+def isfloat(version):
+    """Simple method to verify if a value can be converted to a float"""
+    try:
+        float(version)
+        return True
+    except ValueError:
+        return False
+
+
+def login_binary_path(distro):
+    """Retrieve the login binary path based on the distro version"""
+    detected = distro.detect()
+    if detected.name == "Ubuntu":
+        if float(detected.version) >= 24.04:
+            return "/usr/bin/login"
+    if detected.name == "debian":
+        if isfloat(detected.version):
+            if float(detected.version >= 13):
+                return "/usr/bin/login"
+        if detected.version == "sid":
+            return "/usr/bin/login"
+    return "/bin/login"
+
+
 @unittest.skipUnless(os.getuid() == 0, "This test requires root privileges")
 @unittest.skipUnless(apt_supported_distro(), "Unsupported distro")
-class Apt(unittest.TestCase):
+class Apt(unittest.Test):
     def test_provides(self):
         sm = manager.SoftwareManager()
-        self.assertEqual(sm.provides("/bin/login"), "login")
+        login_path = login_binary_path(distro)
+        self.assertEqual(sm.provides(login_path), "login")
         self.assertTrue(isinstance(sm.backend, backends.apt.AptBackend))
 
 
